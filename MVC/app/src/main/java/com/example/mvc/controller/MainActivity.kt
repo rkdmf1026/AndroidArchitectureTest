69f13718262b16e2da2012a2e8d7e8140028cf57
Lee <rkdmf1026@naver.com>
2022-04-26 16:28:26 +0900

    [DELETE] unnecessary code

--- MVC/app/src/main/java/com/example/mvc/controller/MainActivity.kt
+++ MVC/app/src/main/java/com/example/mvc/controller/MainActivity.kt
@@ -12,45 +12,31 @@ import kotlinx.coroutines.launch
 import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
-
     private lateinit var binding: ActivityMainBinding
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.stationName.text = Editable.Factory.getInstance().newEditable("종로구")
-
-
         binding.btnSearch.setOnClickListener {
             CoroutineScope(Dispatchers.IO).launch {
                 try {
                     val info = MainService.retrofitService.listAir(
-                        com.example.mvc.BuildConfig.airKoreaKey,
                         "json",
+                        com.example.mvc.BuildConfig.airKoreaKey,
                         2,
                         1,
                         binding.stationName.text.toString(),
                         "DAILY",
                         1.0
                     ).body()!!.response.body.items[0]
-                    binding.textTime.text= info.dataTime
+                    binding.textTime.text = info.dataTime
                     binding.pm10.text = info.pm10Value
                     binding.pm10Grade.text = info.pm10Grade
-
                 } catch (e: Exception) {
                     Log.e("network error", e.toString())
-
                 }
             }
-
-
-
-
-
         }
-
-
     }
 }
